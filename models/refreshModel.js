334fb4ae0e145b22e031e7fb58f93a35cff1d786
avishay avraham <avishayavraham555@gmail.com>
2023-02-08 19:19:42 +0200

    delete tokens model

--- models/refreshModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const tokensSchema = new mongoose.Schema({
-   refreshToken:{type:String, required:true}
-})
-
-const model = mongoose.model("tokens", tokensSchema);
-module.exports = model;
